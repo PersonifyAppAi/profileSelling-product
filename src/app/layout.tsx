294c7d6c5e8b0769e24d23869203fc7ab61b0d6f
Misha Gupta <mishagupta@Mishas-MacBook-Air-9.local>
2025-01-26 16:19:43 -0500

    student profile skeleton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className="bg-slate-50"
+        className="bg-slate-50 h-screen"
       >
         <Navbar />
         <main className="px-4 md:px-6">
